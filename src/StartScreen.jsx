4dc06e53dbd0ef5f5f66f3fdae2e21f3645cf6e9
Amnamot <amnamot@gmail.com>
2025-05-06 19:06:00 +0300

    ожидание

--- src/StartScreen.jsx
+++ src/StartScreen.jsx
@@ -3,6 +3,7 @@ import WaitModal from '../components/WaitModal';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import TopUpModal from './components/TopUpModal';
+import WaitModal from './components/WaitModal';
 import './StartScreen.css';
 
 const StartScreen = () => {

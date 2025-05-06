37ad438595d34502eb6e58f5a9009181d7ce0e43
Amnamot <amnamot@gmail.com>
2025-05-06 16:21:06 +0300

    кнопка Start

--- src/StartScreen.jsx
+++ src/StartScreen.jsx
@@ -1,4 +1,4 @@
-// src/StartScreen.jsx v6
+// src/StartScreen.jsx v7
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import TopUpModal from './components/TopUpModal';

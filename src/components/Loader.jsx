3b7b690fae1d09c1aed4034b8172440eeb53b9fd
Amnamot <amnamot@gmail.com>
2025-05-23 12:51:29 +0300

    снова обработка инвайта

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Loader.css';
-import { joinLobby } from '../services/socket';
+import { joinLobby, socket } from '../services/socket';
 
 const Loader = () => {
   const navigate = useNavigate();
